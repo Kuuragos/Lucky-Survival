8838dfd53d10832f06327fb707bfff29b39ff2bd
Kuuragos <alejandrab.19401@gmail.com>
2019-09-27 12:24:12 -0500

    hola

---
+++ Assets/Scripts/Clases/Objetos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objetos : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public enum ObjetosMapa
+    { Revolver}
+}
